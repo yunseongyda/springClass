b65f3b7264a9887c5bc27c95e7e9f8ff86605415
yunseongyda <kys070510@gmail.com>
2025-01-17 19:23:28 +0900

    study JUnit
    Optional : no error occurs if the return is Null
    findBy-- :  find thing what i want from Entity

--- test/src/main/java/com/study/mysite/sbb/QuestionRepository.java
+++ test/src/main/java/com/study/mysite/sbb/QuestionRepository.java
@@ -1,7 +1,13 @@
 package com.study.mysite.sbb;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>{
-
+	// 원래 JpaRepo에는 findBySubject는 없는데 걍 이렇게 쓰면 Spring이 알아서 만들어줌
+	// 매개변수에는 열 이름
+	Question findBySubject(String subject);
+	Question findBySubjectAndContent(String subject, String content);
+	List<Question> findBySubjectLike(String subject);
 }
--- test/src/test/java/com/study/mysite/TestApplicationTests.java
+++ test/src/test/java/com/study/mysite/TestApplicationTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,7 @@ class TestApplicationTests {
 	
 	@Test
 	void testJpa() {
+		/*
 		Question q1 = new Question();
 		q1.setSubject("what is mean of name the Spring boot");
 		q1.setContent("I wanna know how to use Spring boot");
@@ -42,7 +44,30 @@ class TestApplicationTests {
 //		assertEquals(5, all.size());
 		
 		String firstSubject = all.get(0).getSubject();
-		assertEquals("spring", firstSubject);
+//		assertEquals("spring", firstSubject);
+		
+//		Optional : null이 와도 에러 안 냄
+		Optional<Question> q = this.questionRepository.findById(13);
+		if (q.isPresent()) {
+			Question qs = q.get();
+			assertEquals("I love NMIXX", qs.getSubject());
+		}
+		
+		// findBy- 하면 여러개가 들어오면 에러남
+		Question q = this.questionRepository.findBySubject("spring"); 
+//		assertEquals(4, q.getId());
+		
+		Question q = this.questionRepository.findBySubjectAndContent("spring", "I wanna know how to use Spring boot");
+		assertEquals(4, q.getId());
+		
+		List<Question> qList = this.questionRepository.findBySubjectLike("%NMIXX");
+		Question q = qList.get(0);
+		assertEquals(7, q.getId());
+		
+		assertEquals(17, this.questionRepository.count());
+		*/
+		
+		
 	}
 
 }
