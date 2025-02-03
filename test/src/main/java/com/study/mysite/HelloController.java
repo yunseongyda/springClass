0c81ff81c604302260f8746ef073052f627c8fe1
yunseongyda <kys070510@gmail.com>
2025-02-03 09:32:10 +0900

    study Service Layer
    service계층 만들기

--- test/src/main/java/com/study/mysite/HelloController.java
+++ test/src/main/java/com/study/mysite/HelloController.java
@@ -6,19 +6,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-	
+
 	@GetMapping("/hello")
 	@ResponseBody
 	public String helle() {
 		return "hihihihihi";
 	}
-	
-//	ResponseBody는 html파일에 뭔가 띄우는 거 
-//	그냥 GetMapping()만 하면 return하는 페이지로 가짐
-//	그냥 주소 뒤에 / 하면 index로 가지는데 GetMapping에 /만 넣고 return을 main을 주면 index 페이지(기본)가 main 페이지가 됨
+
+	// ResponseBody는 html파일에 뭔가 띄우는 거
+	// 그냥 GetMapping()만 하면 return하는 페이지로 가짐
+	// 그냥 주소 뒤에 / 하면 index로 가지는데 GetMapping에 /만 넣고 return을 main을 주면 index 페이지(기본)가
+	// main 페이지가 됨
 	@GetMapping("/main")
 	public String index() {
 		return "main";
 	}
-	
+
+	@GetMapping("/")
+	public String root() {
+		// return "redirect:/question/list";
+		return "index";
+	}
 }
