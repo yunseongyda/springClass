8d7e5bf70255ba8338b59bb001ee4a4906ec161e
yunseongyda <kys070510@gmail.com>
2025-02-03 10:09:06 +0900

    add question detail page
    click on the question title to go to the question details page

--- test/src/main/java/com/study/mysite/sbb/question/QuestionController.java
+++ test/src/main/java/com/study/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,11 +16,20 @@ public class QuestionController {
 	private final QuestionRepository questionRepository;
 	private final QuestionService questionService;
 
+	// 게시판 리스트로 이동
 	@GetMapping("/question/list")
 	public String list(Model model) {
 		// List<Question> questionList = this.questionRepository.findAll();
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question";
+	}
+
+	// 상세페이지로 이동
+	@GetMapping("/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) { // @PathVariable : url에 있는 값을 가져옴
+		Question question = this.questionService.getQuestion(id);					// Integer타입의 id 컬럼값과 연결하여 @PathVariable("변수명")으로 변경한다. => 사용자 요청 url의 변수명으로 사용 가능
+		model.addAttribute("question", question);
+		return "question_detail";
 	}
 }
--- test/src/main/java/com/study/mysite/sbb/question/QuestionService.java
+++ test/src/main/java/com/study/mysite/sbb/question/QuestionService.java
@@ -1,8 +1,11 @@
 package com.study.mysite.sbb.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.study.mysite.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,5 +16,14 @@ public class QuestionService {
 
   public List<Question> getList() {
     return this.questionRepository.findAll();
+  }
+
+  public Question getQuestion(Integer id) {
+    Optional<Question> question = this.questionRepository.findById(id);
+    if (question.isPresent()) {
+      return question.get();
+    } else {
+      throw new DataNotFoundException("Question not found");
+    }
   }
 }
