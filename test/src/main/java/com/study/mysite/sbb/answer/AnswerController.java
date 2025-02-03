7f9c495b5902abcd3ded3c0cd3e67e931173bc17
yunseongyda <kys070510@gmail.com>
2025-02-03 11:16:04 +0900

    add question detail content
    Print question details

---
+++ test/src/main/java/com/study/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,29 @@
+package com.study.mysite.sbb.answer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.study.mysite.sbb.question.Question;
+import com.study.mysite.sbb.question.QuestionService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+  
+  private final QuestionService questionService;
+  private final AnswerService answerService;
+  
+  @PostMapping("/create/{questionId}")
+  public String createAnswer(Model model, @PathVariable("questionId") Integer id, @RequestParam(value = "content") String content) {
+    Question question = this.questionService.getQuestion(id);
+    this.answerService.create(question, content);
+    return String.format("redirect:/question/detail/%s", id);
+  }
+}
--- test/src/main/java/com/study/mysite/sbb/question/QuestionController.java
+++ test/src/main/java/com/study/mysite/sbb/question/QuestionController.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question") // 이 컨트롤러의 모든 매핑은 /question으로 시작
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
@@ -17,16 +19,16 @@ public class QuestionController {
 	private final QuestionService questionService;
 
 	// 게시판 리스트로 이동
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		// List<Question> questionList = this.questionRepository.findAll();
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
-		return "question";
+		return "question_list";
 	}
 
 	// 상세페이지로 이동
-	@GetMapping("/question/detail/{id}")
+	@GetMapping("/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) { // @PathVariable : url에 있는 값을 가져옴
 		Question question = this.questionService.getQuestion(id);					// Integer타입의 id 컬럼값과 연결하여 @PathVariable("변수명")으로 변경한다. => 사용자 요청 url의 변수명으로 사용 가능
 		model.addAttribute("question", question);
